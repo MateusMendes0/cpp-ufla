6824bcce8818495b2c4094775ad14d39fa1c27f5
Mateus Mendes <91296161+Slndr02@users.noreply.github.com>
2023-05-23 20:46:59 -0300

    Update esparsa.cpp

--- esparsa.cpp
+++ esparsa.cpp
@@ -6,7 +6,13 @@ int main()
 {
     int matriz[10][10], contzero=0;
     
-    
+    for (i=0; i<10; i++){
+		for(j=0; j<10; j++{
+			cin >> matriz[i][j];
+			}
+		}
+	
+	
     for (i=0; i<10; i++){
 		for(j=0; j<10; j++{
 			if (matriz[i][j] == 0){

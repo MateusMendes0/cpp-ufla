c7a8c7fadec6b3ff48d4a7d7988d9767f6ce9475
Mateus Mendes <91296161+Slndr02@users.noreply.github.com>
2023-05-23 09:34:58 -0300

    Add files via upload

---
+++ esparsa.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int matriz[10][10], contzero=0;
+    
+    
+    for (i=0; i<10; i++){
+		for(j=0; j<10; j++{
+			if (matriz[i][j] == 0){
+				contzero++
+				}
+			}
+		}
+	if (contzero >= floor(200/3)){
+		cout << "é esparsa"
+		}
+	else{
+		cout << "não é esparsa"
+		}
+    return 0;
+}
